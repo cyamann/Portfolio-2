fcce78a3114b3f22a7737114c8b191b72db12912
cyamann <cerenyaman321123@gmail.com>
2023-09-19 10:39:53 +0300

    e

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path"); // Import the 'path' module
 const app = express();
-app.use(express.static("public"));
-app.listen(3000, ()=>{
-    console.log("App is running on port 3000");
-}
-)
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
